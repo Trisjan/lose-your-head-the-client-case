5566ffbce34aa353a902e6b77641ff27c0f1389d
Duneya <54691201+Duneyasaleh@users.noreply.github.com>
2023-09-26 13:04:25 +0200

    navigation bar toegevoegd  html

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<nav>
+   <section class="menu-bar">
+    <header class="menu-bar__user-info">
+      <img src="/" alt="" class="menu-bar__user-info--img">
+      <h2 class="menu-bar__user-info--name">user name</h2>
+      <p class="menu-bar__user-info--profession">5 jaar lid</p>
+    </header>
+    <article class="menu-bar__search">
+       <input type="text" placeholder="Zoek...">
+    </article>
+    <article class="menu-bar__list">
+        <ul>
+            <li><a href="/Welkom">Welkom</a></li>
+            <li><a href="/Leeslijst">Leeslijst</a></li>
+            <li><a href="/Uitleningen-en-verlengen">Uitleningen en verlengen</a></li>
+            <li><a href="/Uitleengeschiedenis">Uitleengeschiedenis</a></li>
+            <li><a href="/Betaalgeschiedenis">Betaalgeschiedenis</a></li>
+            <li><a href="/Reserveringen">Reserveringen</a></li>
+            <li><a href="/Desposito's">Desposito's</a></li>
+            <li><a href="/Tellerstand-abonnement">Tellerstand abonnement</a></li>
+        </ul>
+    </article>
+    <article class="menu-bar__content">
+        <ul>
+            <li><a href="/Uitloggen">Uitloggen</a></li>
+            <li><a href="/>Dark-mode">Dark mode</a></li>
+        </ul>
+
+    </article>
+   </section> 
+</nav>
+
+<slot />
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    MIJN OBA
+</body>
+</html>
