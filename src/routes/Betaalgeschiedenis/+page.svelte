2e45e41d718b71bb970b9e8f9af4342319767ba7
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2023-10-02 20:03:43 +0200

    klein begin

---
+++ src/routes/Betaalgeschiedenis/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let data
+    console.log(data)
+</script>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    MIJN OBA
+
+    {#each data.reservations as reservation}
+        <blockquote>
+            <p>{reservation.userName}</p>
+            <p>Your reservation id = {reservation.id}</p>
+        </blockquote>
+    {/each}
+</body>
+</html>
