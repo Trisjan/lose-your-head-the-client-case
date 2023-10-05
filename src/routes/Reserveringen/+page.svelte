711c8fb70e764dd95259369ae93b959d1bd0c7e4
Emona Santiago <emonasantiago1207@gmail.com>
2023-10-05 15:32:08 +0200

    Reserveringen start

--- src/routes/Reserveringen/+page.svelte
+++ src/routes/Reserveringen/+page.svelte
@@ -1,1 +1,82 @@
-<h1> Reserveringen </h1>
+<section id="reserveringen">
+<h1> Mijn Reserveringen </h1>
+<p>Hier vind je jou reseveringslijst.</p>
+
+
+<div style="overflow-x:auto;">
+  <table>
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Points</th>
+      <th>Points</th>
+      <th>Points</th>
+      <th>Points</th>
+      <th>Points</th>
+      <th>Points</th>
+      <th>Points</th>
+      <th>Points</th>
+      <th>Points</th>
+      <th>Points</th>
+    </tr>
+    <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+      <td>50</td>
+    </tr>
+    <tr>
+      <td>Eve</td>
+      <td>Jackson</td>
+      <td>94</td>
+      <td>94</td>
+      <td>94</td>
+      <td>94</td>
+      <td>94</td>
+      <td>94</td>
+      <td>94</td>
+      <td>94</td>
+      <td>94</td>
+      <td>94</td>
+    </tr>
+    <tr>
+      <td>Adam</td>
+      <td>Johnson</td>
+      <td>67</td>
+      <td>67</td>
+      <td>67</td>
+      <td>67</td>
+      <td>67</td>
+      <td>67</td>
+      <td>67</td>
+      <td>67</td>
+      <td>67</td>
+      <td>67</td>
+    </tr>
+  </table>
+</div>
+</section>
+
+<style>
+    :root {
+  background: #E3E9F9;
+}
+
+#reserveringen {
+    background-color: white;
+    padding: 1em;
+    border-radius: 10px;
+}
+
+td {
+    border-color: black;
+}
+</style>
