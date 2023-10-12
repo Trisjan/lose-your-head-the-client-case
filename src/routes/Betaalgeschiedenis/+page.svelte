aada49ee585a9bc62f6e8981133c74dc6649da81
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2023-10-12 16:27:40 +0200

    :(

--- src/routes/Betaalgeschiedenis/+page.svelte
+++ src/routes/Betaalgeschiedenis/+page.svelte
@@ -11,82 +11,81 @@
 </head>
 <body>
 
+    
     <!-- {#each data.reservations as reservation}
         <blockquote>
             <p>{reservation.userName}</p>
             <p>Your reservation id = {reservation.id}</p>
         </blockquote>
-    {/each} -->
-
-
-    <header> 
-        <img src={logo} alt="logo van oba">
-        <h1>Betalingen</h1>
-    </header>
-
-    <section>
-
-          <table>
-            <tr>
-              <th>SOORT BETALING</th>
-              <th>BETAALDATUM</th>
-              <th>BEDRAG</th>
-              <th>BETALINGSWIJZE</th>
-              <th>DETAILS</th>
-            </tr>
-
-        <div> 
-
-            <tr>
-              <td>Contributie</td>
-              <td>01/08/2023</td>
-              <td>€19.99</td>
-              <td>PIN</td>
-              <td>7394628495</td>
-            </tr>
-            <tr>
-                <td>Betaling uit deposito</td>
-                <td>21/07/2023</td>
-                <td>€25.00</td>
-                <td>PIN</td>
-                <td>9749937459</td>
-              </tr>
-            <tr>
-                <td>Boete</td>
-                <td>21/07/2023</td>
-                <td>€2.75</td>
-                <td>Contant</td>
-                <td>3845928503</td>
-            </tr>
-            <tr>
-                <td>Inschrijving</td>
-                <td>01/06/2023</td>
-                <td>€1.00</td>
-                <td>PIN</td>
-                <td>8648543578</td>
-            </tr>
-            <tr>
-                <td>Verkopen</td>
-                <td>01/07/2023</td>
-                <td>€2.99</td>
-                <td>PIN</td>
-                <td>9537905742</td>
-            </tr>
-
-        </div>       
-
-          </table>
-
-    </section>
-
-    <article>
+        {/each} -->
         
-    </article>
+        
+        <header> 
+            <img src={logo} alt="logo van oba">
+            <h1>Betalingen</h1>
+        </header>
+        
+        <main>
+
+            <section>
+            
+             <ul class="ul-titles">
+                 <li>SOORT BETALING</li>
+                 <li>BETAALDATUM</li>
+                 <li>BEDRAG</li>
+                 <li>BETALINGSWIJZE</li>
+                 <li>DETAILS</li>
+             </ul>
+             
+             <div>
+                 
+             <ul>
+                 <li>Contributie</li>
+                 <li>01/08/2023</li>
+                 <li>€19.99</li>
+                 <li>PIN</li>
+                 <li>7394628495</li>
+             </ul>
+             <ul>
+                 <li>Betaling uit deposito</li>
+                 <li>21/07/2023</li>
+                 <li>€25.00</li>
+                 <li>PIN</li>
+                 <li>9749937459</li>
+               </ul>
+             <ul>
+                 <li>Boete</li>
+                 <li>21/07/2023</li>
+                 <li>€2.75</li>
+                 <li>Contant</li>
+                 <li>3845928503</li>
+             </ul>
+             <ul>
+                 <li>Inschrijving</li>
+                 <li>01/06/2023</li>
+                 <li>€1.00</li>
+                 <li>PIN</li>
+                 <li>8648543578</li>
+             </ul>
+             <ul>
+                 <li>Verkopen</li>
+                 <li>01/07/2023</li>
+                 <li>€2.99</li>
+                 <li>PIN</li>
+                 <li>9537905742</li>
+             </ul>
+
+             </div>     
+
+            </section>
+                
+</main>
 
 </body>
 </html>
 
 <style>
+
     @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&family=Poppins:wght@600&display=swap');
     
     *{ margin: 0;
@@ -97,23 +96,38 @@
         background: #E4E9F7;
     }
 
+    section{
+        width: 60rem;
+        display: flex;
+        flex-direction: column;
+    }
+    
+    main{
+        overflow-x: scroll;
+    }
+
     img{
         height: 2rem;
     }
 
-    section{
-        overflow-x: auto
+    ul{
+        display: inline-flex;
+        flex-direction: column;
+        /* display: flex;
+        justify-content: space-between; */
+        list-style: none;
+    }
+
+    .ul-titles{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
     }
 
     div{
-        position: absolute;
-        background: white;
-        border-radius: 1rem;
-        padding: 1rem;
-    }
-
-    td, th{
-        padding: 0 2rem  ;
+       background: white;
+       border-radius: 1rem;
+       justify-content: space-between;
     }
 
 </style>
